1049f87aed052b6354170e117920c936fd740b09
81andie <133395577+81andie@users.noreply.github.com>
2024-03-29 17:29:51 +0100

    Construyendo seccion información

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { Header } from './Components/Header';
 import { Cabecera } from './Components/Cabecera';
+import { SeccionInformativa } from './Components/SeccionInformativa';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Cabecera/>
+      <SeccionInformativa/>
       
     </div>
   );
---
+++ src/Components/SeccionInformativa.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+export const SeccionInformativa = () => {
+    return (
+
+        <>
+
+
+
+            <div className="seccionInformacion">
+
+                <div className='icon_section'>
+
+                    <span><i className="pi pi-thumbs-up" style={{ color: 'slateblue' }}></i></span>
+                    <h1 className="titsec_Informacion">100% profesionalidad</h1>
+                    <p> faucibus aliquet. Duis sollicitudin orci sit amet dignissim viverra. Morbi finibus, nulla sit amet tincidunt tincidunt, nisl leo rutrum ante, sed venenatis erat orci eu sem. </p>
+
+                </div>
+
+                <div className='icon_section'>
+
+                    <span><i className="pi pi-thumbs-up" style={{ color: 'slateblue' }}></i></span>
+                    <h1 className="titsec_Informacion">100% profesionalidad</h1>
+                    <p> faucibus aliquet. Duis sollicitudin orci sit amet dignissim viverra. Morbi finibus, nulla sit amet tincidunt tincidunt, nisl leo rutrum ante, sed venenatis erat orci eu sem. </p>
+
+                </div>
+                
+                <div className='icon_section'>
+
+                    <span><i className="pi pi-thumbs-up" style={{ color: 'slateblue' }}></i></span>
+                    <h1 className="titsec_Informacion">100% profesionalidad</h1>
+                    <p> faucibus aliquet. Duis sollicitudin orci sit amet dignissim viverra. Morbi finibus, nulla sit amet tincidunt tincidunt, nisl leo rutrum ante, sed venenatis erat orci eu sem. </p>
+
+                </div>
+
+
+
+
+        </div>
+
+
+              
+
+               
+
+        </>
+    )
+}
