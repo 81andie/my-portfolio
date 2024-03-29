d7c80d82981065325493e01152141f26f14af3c2
81andie <133395577+81andie@users.noreply.github.com>
2024-03-29 18:05:27 +0100

    Cambios en la seccion informacion

--- src/Components/SeccionInformativa.jsx
+++ src/Components/SeccionInformativa.jsx
@@ -13,23 +13,23 @@ export const SeccionInformativa = () => {
 
                     <span><i className="pi pi-thumbs-up" style={{ color: 'slateblue' }}></i></span>
                     <h1 className="titsec_Informacion">100% profesionalidad</h1>
-                    <p> faucibus aliquet. Duis sollicitudin orci sit amet dignissim viverra. Morbi finibus, nulla sit amet tincidunt tincidunt, nisl leo rutrum ante, sed venenatis erat orci eu sem. </p>
+                    <p> faucibus aliquet. Duis sollicitudin orci sit amet dignissim viverra.faucibus aliquet. Duis sollicitudin orci sit amet dignissim viverra. Morbi finibus, nulla sit amet tincidunt tincidunt, nisl leo rutrum ante, sed venenatis erat orci eu sem. </p>
 
                 </div>
 
                 <div className='icon_section'>
 
-                    <span><i className="pi pi-thumbs-up" style={{ color: 'slateblue' }}></i></span>
-                    <h1 className="titsec_Informacion">100% profesionalidad</h1>
-                    <p> faucibus aliquet. Duis sollicitudin orci sit amet dignissim viverra. Morbi finibus, nulla sit amet tincidunt tincidunt, nisl leo rutrum ante, sed venenatis erat orci eu sem. </p>
+                    <span><i className="pi pi-question" style={{ color: 'slateblue' }}></i></span>
+                    <h1 className="titsec_Informacion">Te asesoro para escoger la mejor opción para tu empresa</h1>
+                    <p> faucibus aliquet.Duis sollicitudin orci sit amet dignissim viverra. Morbi finibus, Duis sollicitudin orci sit amet dignissim viverra. Morbi finibus, nulla sit amet tincidunt tincidunt, nisl leo rutrum ante, sed venenatis erat orci eu sem. </p>
 
                 </div>
-                
+
                 <div className='icon_section'>
 
-                    <span><i className="pi pi-thumbs-up" style={{ color: 'slateblue' }}></i></span>
-                    <h1 className="titsec_Informacion">100% profesionalidad</h1>
-                    <p> faucibus aliquet. Duis sollicitudin orci sit amet dignissim viverra. Morbi finibus, nulla sit amet tincidunt tincidunt, nisl leo rutrum ante, sed venenatis erat orci eu sem. </p>
+                    <span><i className="pi pi-tablet" style={{ color: 'slateblue' }}></i></span>
+                    <h1 className="titsec_Informacion">Tu página web en cualquier dispositivo</h1>
+                    <p> faucibus aliquet.Duis sollicitudin orci sit amet dignissim viverra. Morbi finibus, Duis sollicitudin orci sit amet dignissim viverra. Morbi finibus, nulla sit amet tincidunt tincidunt, nisl leo rutrum ante, sed venenatis erat orci eu sem. </p>
 
                 </div>
 
