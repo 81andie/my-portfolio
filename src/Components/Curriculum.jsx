e55b90743c0759b6ad1738945984cd6f7bc8e442
81andie <133395577+81andie@users.noreply.github.com>
2024-03-30 20:02:31 +0100

    Empezando a esbozar la sección de Skills

--- src/Components/Curriculum.jsx
+++ src/Components/Curriculum.jsx
@@ -3,9 +3,46 @@ import React from 'react'
 
 export const Curriculum = () => {
   return (
-    <div>Curriculum
+    <>
+
+      <h1>Skills</h1>
+      <div className="contenedor">
+
+       <figure>
+        <img src="https://miro.medium.com/max/1200/0*2JYJbAv70Asvm2de.png"></img>
+        <div className="capa">
+          <h2>100%</h2>
+          <p>Dominio profundo, del lenguaje de marcado, lo que me permite crear y estructurar contenido, de una manera clara y concisa</p>
+
+        </div>
       
-        <h1>Curriculum</h1>
-    </div>
+       </figure>
+
+       <figure>
+        <img src="https://th.bing.com/th/id/OIP.xEVLs9u_Z6fr-jKrDpWI6AHaD6?rs=1&pid=ImgDetMain"></img>
+        <div className="capa">
+          <h2>100%</h2>
+          <p>Creo y diseño interfaces web con estilo y elegancia, sin olvidar el responsive en todos los dispositivos. </p>
+
+        </div>
+      
+       </figure>
+
+
+       <figure>
+        <img src="https://blog.faradars.org/wp-content/uploads/2023/01/what-is-JS.jpg"></img>
+        <div className="capa">
+          <h2>70%</h2>
+          <p>Conocimiento sólido, de las principales funcionalidades de Javascript, y sigo en continuo aprendizaje</p>
+
+        </div>
+      
+       </figure>
+       
+        
+      </div>
+
+
+    </>
   )
 }
