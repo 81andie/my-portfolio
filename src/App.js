77fe1b0f0853fd57b30a423b9e848d40435af2e0
81andie <133395577+81andie@users.noreply.github.com>
2024-04-05 19:55:38 +0200

    Pruebas con dark theme

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import './App.css';
 import './responsive.css'
+import {useState} from 'react';
 import { RoutasPrincipales } from './routing/RoutasPrincipales';
 
 
 
 function App() {
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  };
+
+
   return (
+    
     <div className="App">
+
+      <div className= {theme === 'light' ? 'light-theme' : 'dark-theme'}>
+      <button className={theme} onClick={toggleTheme}>Cambiar theme</button>
+
       <RoutasPrincipales/>
     
-   
+   </div>
     </div>
+
+ 
   );
 }
 
