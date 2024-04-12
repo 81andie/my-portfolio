891ad04b779901008ac2c3ba9d98fccae76d667d
81andie <133395577+81andie@users.noreply.github.com>
2024-04-12 20:26:09 +0200

    Prueba con efecto parallax

--- src/Components/Certificaciones.jsx
+++ src/Components/Certificaciones.jsx
@@ -3,10 +3,67 @@ import React from 'react'
 export const Certificaciones = () => {
   return (
     <>
-    <div className="container_certificaciones">
+      <div className="container_certificaciones">
+        <h1 className="certificaciones_title">Certificaciones</h1>
+        <div className="card_google">
+          <h2>Junio 2023</h2>
+          <h3>Curso de instroducción <br /> al desarrollo web: HTML y CSS</h3>
+          <img src="https://www.emprendedorinteligente.com/wp-content/uploads/2019/11/header-activate-logo-lg-1024x286.png" className="logo_google" />
+          <details>
+            <summary>Sumario</summary>
+            <li>Historia de la Web</li>
+            <li>Como funciona la Web</li>
+            <li>Como se escribe la Web</li>
+            <li>Como se publica un sitio Web</li>
+            <li>Como se escribe una página Web</li>
+          </details>
 
+        </div>
 
-    </div>
+        <div className="card_google1">
+          <h2>Julio 2023</h2>
+          <h3>Curso de instroducción <br /> al desarrollo web: HTML y CSS</h3>
+          <img src="https://www.emprendedorinteligente.com/wp-content/uploads/2019/11/header-activate-logo-lg-1024x286.png" className="logo_google" />
+          <details>
+            <summary>Sumario</summary>
+            <li>Cómo se define la presentación de una página web</li>
+            <li>Cómo se define la presentación de los elementos de una página web</li>
+            <li>Como se logra que una página web tenga éxito</li>
+            <li>Como se maqueta una página web</li>
+          </details>
+
+        </div>
+
+        <div className="card_google3">
+          <h2>Agosto 2023</h2>
+          <h3>Build Responsive Real World <br /> Websites with HTML AND CSS (Udemy)</h3>
+          <img src="https://logosmarcas.net/wp-content/uploads/2021/11/Udemy-Logo-650x366.png" className="logo_google" />
+          <details>
+            <summary>Sumario</summary>
+            <li>Fundamentos html</li>
+            <li>Fundamentos CSS</li>
+            <li>Layouts</li>
+            <li>Componentes</li>
+            <li>Proyecots</li>
+          </details>
+          </div>
+
+        <div className="card_google2">
+          <h2>Setiembre 2023</h2>
+          <h3>Responsive Web Design <br /> Developer Certification</h3>
+          <img src="https://www.arnoldgelacio.com/images/skills/logo-freecodecamp.png" className="logo_google" />
+          <details>
+            <summary>Sumario 300h</summary>
+            <li>Build a Survey Form</li>
+            <li>Build a tribute page</li>
+            <li>Build a Technical Documentation Page</li>
+            <li>Build a Personal Portfolio Page</li>
+          </details>
+
+        </div>
+      
+
+      </div>
     </>
   )
 }
