6d74fae86f338af8dbb8592e9f776326c5a95788
81andie <133395577+81andie@users.noreply.github.com>
2024-04-13 22:28:53 +0200

    cambios en seccion

--- src/Components/Sobremi.jsx
+++ src/Components/Sobremi.jsx
@@ -5,20 +5,26 @@ import React from 'react'
 export const Sobremi = () => {
 
   let gift = "img/Generated_1693910185 (1).jpg";
-
+ 
 
 
   return (
     <>
       <section className="sobremi">
 
-      <img src={gift} className="imge_sobremi"/>
+        <div className="prueba_parallax">
+        <img src={gift} className="imge_sobremi"/>
+     
+     
+        </div>
+
+      
       
         <div className="parallax">
 
-          <h2>Hola, me llamo Andrea, una apasionada del desarrollo web autodidacta.
-            Resido en Gerona. <br/><br/>Desde que descubrí este mundo, hace aproximadamente un año, me he sumergido
-            en este viaje emocionante de aprendizaje constante  y crecimiento personal, <br />en él he aprendido que no hay nada imposible
+          <h2 className="presentación">Hola, me llamo Andrea, una apasionada del desarrollo web autodidacta.
+            Resido en Gerona. Desde que descubrí este mundo, hace aproximadamente un año, me he sumergido
+            en este viaje emocionante de aprendizaje constante  y crecimiento personal,en él he aprendido que no hay nada imposible
             de conseguir si trabajas duro y con disciplina</h2>
 
 
