5bbdaa95825a71587548d23fd023ef900dd590f9
81andie <133395577+81andie@users.noreply.github.com>
2024-04-15 18:38:32 +0200

    Cambios para configurar la traducción a diferentes idiomas de la sección habilidades

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
-
+import { useTranslation } from 'react-i18next';
 
 export const Skills = () => {
+  const { t } = useTranslation();
+
   return (
     <>
 
-      <h1 className="skills">Skills</h1>
+      <h1 className="skills">{t('header.skills')}</h1>
       <div className="contenedor">
 
         <figure>
           <img src="https://miro.medium.com/max/1200/0*2JYJbAv70Asvm2de.png" alt="img-skills"></img>
           <div className="capa">
             <h2>100%</h2>
-            <p>Dominio profundo, del lenguaje de marcado, lo que me permite crear y estructurar contenido, de una manera clara y concisa</p>
+            <p>{t('skills.markupLanguage')}</p>
 
           </div>
 
@@ -22,7 +24,7 @@ export const Skills = () => {
           <img src="https://th.bing.com/th/id/OIP.xEVLs9u_Z6fr-jKrDpWI6AHaD6?rs=1&pid=ImgDetMain" alt="img-skills"></img>
           <div className="capa">
             <h2>100%</h2>
-            <p>Creo y diseño interfaces web con estilo y elegancia, sin olvidar el responsive en todos los dispositivos. </p>
+            <p>{t('skills.webInterfaceDesign')} </p>
 
           </div>
 
@@ -33,7 +35,7 @@ export const Skills = () => {
           <img src="https://blog.faradars.org/wp-content/uploads/2023/01/what-is-JS.jpg" alt="img-skills"></img>
           <div className="capa">
             <h2>70%</h2>
-            <p>Conocimiento sólido, de las principales funcionalidades de Javascript, y sigo en continuo aprendizaje</p>
+            <p>{t('skills.javascriptKnowledge')}</p>
 
           </div>
 
@@ -45,7 +47,7 @@ export const Skills = () => {
           <img src="https://theomnibuzz.com/wp-content/uploads/2020/12/react-js-1_orig.png" alt="img-skills"></img>
           <div className="capa">
             <h2>70%</h2>
-            <p>Actualmente llevo, varios meses perfeccionandome en el domino de React, esta página mismo esta construida en React</p>
+            <p>{t('skills.reactExperience')}</p>
 
           </div>
 
@@ -57,7 +59,7 @@ export const Skills = () => {
           <img src="https://www.filepicker.io/api/file/7vjtVUXBQHyTO4AhJ7mW" alt="img-skills" ></img>
           <div className="capa">
             <h2>50%</h2>
-            <p>Mi experiencia abarca los aspectos fundamentales, como creación de DB y colecciones, diseño de consultas básicas y manipulación de documentos </p>
+            <p>{t('skills.databaseExperience')}</p>
 
           </div>
 
@@ -67,7 +69,7 @@ export const Skills = () => {
           <img src="https://www.themeinthebox.com/wp-content/uploads/elementor/thumbs/Bootstrap-5-q6cc37pc8ekqe5d3vc9scpfvtcdjqj6ei5n4rcfucw.png" alt="img-skills"></img>
           <div className="capa">
             <h2>100%</h2>
-            <p>Buen conocimiento de Boostrap, suelo utilizarlo en bastantes proyectos</p>
+            <p>{t('skills.bootstrapKnowledge')}</p>
 
           </div>
 
@@ -77,7 +79,7 @@ export const Skills = () => {
           <img src="https://th.bing.com/th/id/OIP.piAMkLAjuBhL3mIPbPgROgHaDm?rs=1&pid=ImgDetMain" alt="img-skills" ></img>
           <div className="capa">
             <h2>90%</h2>
-            <p>En mi dia a dia, no puedo prescindir de él, me ha acompañado en todo esta aventura, el 100% de mi tiempo</p>
+            <p>{t('skills.dailyUsage')}</p>
 
           </div>
 
@@ -87,7 +89,7 @@ export const Skills = () => {
           <img src="https://miro.medium.com/max/3840/1*fDwzjCH3qzhosC6DVel4ng.jpeg" alt="img-skills"></img>
           <div className="capa">
             <h2>90%</h2>
-            <p>Al igual que Github, Git siempre ha estado en mi curso de aprendizaje, el estudio a sido diario</p>
+            <p>{t('skills.gitExperience')}</p>
 
           </div>
 
@@ -99,7 +101,7 @@ export const Skills = () => {
           <img src="https://freecodestack.com/wp-content/uploads/2022/12/What-is-tailwind-CSS-and-its-features.jpg"></img>
           <div className="capa">
             <h2>80%</h2>
-            <p> He trabajado asiduamente con Tailwind, esto me a proporcionado un dominio profundo y equilibrado de este framework</p>
+            <p> {t('skills.tailwindExperience')}</p>
 
           </div>
 
