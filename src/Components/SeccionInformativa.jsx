8e2988e005efa8e5ad978116322400e4e629f559
81andie <133395577+81andie@users.noreply.github.com>
2024-04-15 12:56:35 +0200

    Cambios en sección de información.

--- src/Components/SeccionInformativa.jsx
+++ src/Components/SeccionInformativa.jsx
@@ -14,7 +14,7 @@ export const SeccionInformativa = () => {
 
                     <span><i className="pi pi-thumbs-up" style={{ color: 'rgba(35, 158, 224, 0.63)' }}></i></span>
                     <h1 className="titsec_Informacion">100% profesionalidad</h1>
-                    <p className="parrsec_Informacion"> Me comprometo a ofrecerte un servicio de calidad y profesionalidad excepcional, en la creación de tu página web idea. Estoy aquí para brindarte una experiencia de diseño web y de funcionalidad, rápida, precisa y adaptada a tus necesidades específicas </p>
+                    <p className="parrsec_Informacion"> Me comprometo a ofrecerte un servicio de calidad y profesionalidad excepcional, en la creación de tu página web. Estoy aquí para brindarte una experiencia de diseño web y de funcionalidad, rápida, precisa y adaptada a todas tus necesidades</p>
 
                 </div>
 
@@ -22,7 +22,7 @@ export const SeccionInformativa = () => {
 
                     <span><i className="pi pi-question" style={{ color: 'rgba(35, 158, 224, 0.63)' }}></i></span>
                     <h1 className="titsec_Informacion">Te asesoro para escoger la mejor opción para tu empresa</h1>
-                    <p className="parrsec_Informacion"> Estoy aquí para ayudarte a encontrar la mejor solución para tu proyecto. Si necesitas orientación sobre las mejores opciones disponibles estaré encantada de ofrecerte mi mejor solución para ti y tu trabajo </p>
+                    <p className="parrsec_Informacion"> Estoy aquí para ayudarte a encontrar la mejor solución para tu proyecto. Si necesitas orientación sobre las mejores opciones disponibles estaré encantada de ofrecerte mi mejor opción para ti y tu trabajo, lo mismo si ya tienes una página web y quieres adaptar un nuevo diseño o funcionalidad. </p>
 
                 </div>
 
@@ -30,7 +30,7 @@ export const SeccionInformativa = () => {
 
                     <span><i className="pi pi-tablet" style={{ color: 'rgba(35, 158, 224, 0.63)' }}></i></span>
                     <h1 className="titsec_Informacion">Tu página web en cualquier dispositivo</h1>
-                    <p className="parrsec_Informacion"> Trabajo diligentemente para asegurarme que cada elemento de tu página web esté perfectamente optimizado y sea adaptable a diferentes tamaños de pantalla. El diseño responsive de alta calidad se adapta fluidamente a cualquier dispositivo</p>
+                    <p className="parrsec_Informacion"> Trabajo diligentemente para asegurarme que cada elemento de tu página web esté perfectamente optimizado y sea adaptable a diferentes tamaños de pantalla. Hoy en día, navegamos hacia la consulta te todo hacia los dispositivos móbiles, por eso es importante dedicarle una parte del desarrollo al diseño responsive. </p>
                 </div>
 
 
