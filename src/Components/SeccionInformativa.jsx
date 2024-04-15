c21dd769d020cd6f5483b440776acc4201e9565e
81andie <133395577+81andie@users.noreply.github.com>
2024-04-15 18:20:25 +0200

    Sección información configurar la traducción

--- src/Components/SeccionInformativa.jsx
+++ src/Components/SeccionInformativa.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next';
 
 
 export const SeccionInformativa = () => {
+
+    const { t } = useTranslation();
+
     return (
 
         <>
@@ -11,24 +15,24 @@ export const SeccionInformativa = () => {
                 <div className='icon_section'>
 
                     <span><i className="pi pi-thumbs-up" style={{ color: 'rgba(35, 158, 224, 0.63)' }}></i></span>
-                    <h1 className="titsec_Informacion">100% profesionalidad</h1>
-                    <p className="parrsec_Informacion"> Me comprometo a ofrecerte un servicio de calidad y profesionalidad excepcional, en la creación de tu página web. Estoy aquí para brindarte una experiencia de diseño web y de funcionalidad, rápida, precisa y adaptada a todas tus necesidades</p>
+                    <h1 className="titsec_Informacion">{t('sectionInformation.professionalism.title')}</h1>
+                    <p className="parrsec_Informacion">{t('sectionInformation.professionalism.description')}</p>
 
                 </div>
 
                 <div className='icon_section'>
 
                     <span><i className="pi pi-question" style={{ color: 'rgba(35, 158, 224, 0.63)' }}></i></span>
-                    <h1 className="titsec_Informacion">Te asesoro para escoger la mejor opción para tu empresa</h1>
-                    <p className="parrsec_Informacion"> Estoy aquí para ayudarte a encontrar la mejor solución para tu proyecto. Si necesitas orientación sobre las mejores opciones disponibles estaré encantada de ofrecerte mi mejor opción para ti y tu trabajo, lo mismo si ya tienes una página web y quieres adaptar un nuevo diseño o funcionalidad. </p>
+                    <h1 className="titsec_Informacion">{t('sectionInformation.advice.title')}</h1>
+                    <p className="parrsec_Informacion">{t('sectionInformation.advice.description')}</p>
 
                 </div>
 
                 <div className='icon_section'>
 
                     <span><i className="pi pi-tablet" style={{ color: 'rgba(35, 158, 224, 0.63)' }}></i></span>
-                    <h1 className="titsec_Informacion">Tu página web en cualquier dispositivo</h1>
-                    <p className="parrsec_Informacion"> Trabajo diligentemente para asegurarme que cada elemento de tu página web esté perfectamente optimizado y sea adaptable a diferentes tamaños de pantalla. Hoy en día, navegamos hacia la consulta te todo hacia los dispositivos móbiles, por eso es importante dedicarle una parte del desarrollo al diseño responsive. </p>
+                    <h1 className="titsec_Informacion">{t('sectionInformation.devices.title')}</h1>
+                    <p className="parrsec_Informacion"> {t('sectionInformation.devices.description')}</p>
                 </div>
 
         </div>
