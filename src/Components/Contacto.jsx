221ecff0d47e79cb1cdccc3a99d8c634197c2f6f
81andie <133395577+81andie@users.noreply.github.com>
2024-04-21 14:48:50 +0200

    cambios en seccion contacto y pruebas no definitivas

--- src/Components/Contacto.jsx
+++ src/Components/Contacto.jsx
@@ -1,19 +1,6 @@
 import React from 'react'
-import { useState, useEffect } from 'react';
 
 export const Contacto = () => {
-  const [scrollPosition, setScrollPosition] = useState(0);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setScrollPosition(window.scrollY);
-    };
-
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
 
 
 
@@ -21,29 +8,29 @@ export const Contacto = () => {
 
 
     <>
-    <form className="register-form" >
+      <form className="register-form" >
 
-    <h1 className="form_title">Si quieres la página de tus sueños, déjame tu correo, <br/>me pondré en contacto contigo, juntos encontraremos la fórmula</h1>
-         
-         
-         <div className="form-group">
-         <div className="img_src" style={{ transform: `translate(-${scrollPosition / 3}px, ${scrollPosition / 2}px)` }}>
-  {/* Contenido del div aquí */}
-</div>
+        <h1 className="form_title">Si quieres la página de tus sueños, déjame tu correo, <br />me pondré en contacto contigo, juntos encontraremos la fórmula</h1>
 
 
-              <label htmlFor="name">Nombre</label>
-              <input type="text" name="name"  />
-            </div>
+        <div className="form-group">
+          <div className="img_src" >
+            {/* Contenido del div aquí */}
+          </div>
 
-            <div className="form-group">
-              <label htmlFor="nick">Correo electrónico</label>
-              <input type="email" name="email"  />
-            </div>
 
-            <input type="submit" value="Registrate" className='btn-Registrate' />
-            
-            </form>
-            </>
+          <label htmlFor="name">Nombre</label>
+          <input type="text" name="name" />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="nick">Correo electrónico</label>
+          <input type="email" name="email" />
+        </div>
+
+        <input type="submit" value="Registrate" className='btn-Registrate' />
+
+      </form>
+    </>
   )
 }
