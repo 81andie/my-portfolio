0c5146ffb87a5b9a34403706f71b2749c725ea69
81andie <133395577+81andie@users.noreply.github.com>
2024-05-06 19:50:21 +0200

    pruebas  y retoques en el footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,20 +6,28 @@ export const Footer = () => {
   return (
     <>
       <div className="footer">
-        <img src={img} className='logo_footer' alt="logo-imagen" />
+      <img src={img} className='logo_footer' alt="logo-imagen" />
 
         <div className="icons_social">
-
+      
           <i className="pi pi-instagram" style={{ color: 'rgba(35, 158, 224, 0.817)' }}></i>
           <i className="pi pi-facebook" style={{ color: 'rgba(35, 158, 224, 0.817)' }}></i>
           <i className="pi pi-github" style={{ color: 'rgba(35, 158, 224, 0.817)' }}></i>
           <i className="pi pi-telegram" style={{ color: 'rgba(35, 158, 224, 0.817)' }}></i>
-        </div>
-        <h4>Diseñadora página web y desarrollor Web:</h4>
-        <h3>Andrea Hernández</h3>
-        <i className="pi pi-envelope" style={{ color: 'rgba(35, 158, 224, 0.817)' }}> pianistgirl81@gmail.com</i>
+          </div>
 
-        <h5> ©2024 Andrea Hernández | All Rights Reserved <br />Todos los derechos reservados</h5>
+        <div className="contacto_footer">
+
+        <h3 className="dev_nombre"> Diseñadora página web y desarrollor Web:<br/> Andrea Hernández <br/>©2024 Andrea Hernández | All Rights Reserved </h3>
+        
+        
+        <i className="pi pi-envelope" style={{ color: 'rgba(35, 158, 224, 0.817)' }}></i>
+        <h2>pianistgirl81@gmail.com</h2>
+
+       
+        </div>
+       
+      
 
       </div>
 
