e135bb3428ed4402498529c70061d549728eab99
81andie <133395577+81andie@users.noreply.github.com>
2024-12-27 22:35:34 +0100

    Probando nuevos estilos en la sección de sobreMi

--- src/Components/Sobremi.jsx
+++ src/Components/Sobremi.jsx
@@ -9,22 +9,31 @@ export const Sobremi = () => {
 
   return (
     <>
-      <section className="flex flex-col justify-center items-center">
-      <div className="w-4/12 flex flex-row justify-center items-center bg-slate-200 rounded-sm p-2">
-       <span className="font-mono">Quién Soy</span>
-        <img src={gift} className="w-20 h-20 object-contain" />
+      <section className="flex flex-col justify-center  md:flex-row lg:flex-row ml-1">
+      <div className="flex flex-row justify-center rounded-sm p-4 bg-gray-200">
+       <span className="font-mono text-2xl">Quién Soy</span>
+        <img src={gift} className="w-16 h-16 object-contain bg-slate-950 rounded-full ml-2" />
+      <div className="flex flex-col justify-center ml-2">
+      <h1 className="font-bold text-2xl">FRONTEND STACK</h1>
+      <h1 className="font-mono"> + de <span class="text-2xl">1</span> año desarrollando webs</h1>
+      <h1 className="font-bold text-xl">React</h1>
+      <h1 className="font-bold text-xl">Angular</h1>
+      </div>
       </div>
      
-      <section className="">
-          <h2 className="">Soy una gerundense que desde hace un tiempo se ha adentrado en el desarrollo web del desarrollo web. Me he formado en esta profesión de manera autodidactica, en parte y grácias a la ayuda de dos mentores y excelentes profesores en Inginiera Informática y Desarrollo Web, a ellos les debo todo lo aprendido, han sido mi guia y referente para mejorar en todos los aspectos necesarios de esta profesión.</h2>
+    
+    
+
       </section>
 
-      <section className="">
-      <span className="">Un mundo descubierto</span><br/>
+      <div className="mt-3 ml-2">
+      <span className="font-mono text-2xl text-white bg-slate-950">Un mundo descubierto</span><br/>
         <h2 className=""> Desde que descubrí este mundo, hace aproximadamente un año, me he sumergido
           en este viaje emocionante de aprendizaje constante  y crecimiento personal,en él he aprendido que no hay nada imposible
           de conseguir si trabajas duro y con disciplina. Con el paso de los meses, la curiosidad y el afán de aprender, han sido el motor incansable que me ha llevado hasta este camino. </h2>
-      </section>
+      </div>
+
+
 
       <section className="">
       <span className="">Viaje entre lenguages</span><br/>
@@ -35,7 +44,7 @@ export const Sobremi = () => {
       <span className="">Grácias por tu visita</span><br/>
         <h2 className=""> Y antes de que te vayas, quiero expresarte mi más sincero agradecimiento por visitar mi portfolio. Cada "click" y cada minuto que pasas aquí es una muestra de confianza y aprecio hacia mi trabajo. Prometo seguir en esta línea, comprometida arduamente para ofrecer contenido de calidad y experiencias únicas.Grácias por formar parte de ello y por creer en mi visión.</h2>
       </section>
-     </section>
+     
 
     </>
   )
