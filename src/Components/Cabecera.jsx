20a13971a2dad815874e9c010552241ddf38f89b
81andie <133395577+81andie@users.noreply.github.com>
2025-01-02 22:25:27 +0100

    Añadiendo Animaciones al figcaption y un bg-gradient

--- src/Components/Cabecera.jsx
+++ src/Components/Cabecera.jsx
@@ -54,32 +54,31 @@ export const Cabecera = () => {
 <div class="grid grid-cols-2 md:grid-cols-4 gap-4 ml-10 mr-10">
     <div class="grid gap-4">
         <figure class="max-w-full relative">
-            <img class="h-auto max-w-full rounded-lg bg-zinc-200" src="public/img/iPhone 15 Mockup, Perspective.png" alt=""/>
-            <figcaption class="bottom-3 w-full z-90 absolute text-center  text-white font-semibold bg-black/50 px-5 py-2 text-xs">SpairAirTracker</figcaption>
+            <img class="h-auto max-w-full rounded-lg opacity-60  hover:opacity-100 transition-opacity duration-300 bg-zinc-200" src="public/img/iPhone 15 Mockup, Perspective.png" alt=""/>
+            <figcaption class="span_maqEscribir2 overflow-hidden rounded-md top-1 w-full z-90 absolute bg-gradient-to-r from-zinc-50  text-zinc-700 font-semibold  px-5 py-2 text-xs ">SpaceAirTracker</figcaption>
         </figure>
         
         <figure class="max-w-full relative"> 
-            <img class="h-auto max-w-full rounded-lg" src="public/img/mockDrop_Thunderbolt display on a table.jpg" alt=""/>
-            <figcaption class="bottom-3 w-full z-90 absolute text-center  text-white font-semibold bg-black/50 px-5 py-2 text-xs">Cleopatra Cosmétics</figcaption>
-        
+            <img class="h-48 object-cover max-w-full  opacity-40 hover:opacity-100 rounded-lg" src="public/img/mockDrop_Thunderbolt display on a table.jpg" alt=""/>
+            <figcaption class="span_maqEscribir2 overflow-hidden rounded-md bottom-2  w-full z-90 absolute bg-gradient-to-r from-zinc-50  text-zinc-700 font-semibold  px-5 py-2 text-xs">Cleopatra Cosmétics</figcaption>
         </figure>
         <figure class="max-w-full relative">
-            <img class="h-auto max-w-full rounded-lg" src="public/img/mockDrop_Apple Pencil close by (5).jpg" alt=""/>
-            <figcaption class="bottom-3 w-full z-90 absolute text-center  text-white font-semibold bg-black/50 px-5 py-2 text-xs">Harry Potter App</figcaption>
+            <img class="h-auto max-w-full rounded-lg opacity-60  hover:opacity-100 transition-opacity duration-300" src="public/img/mockDrop_Apple Pencil close by (5).jpg" alt=""/>
+            <figcaption class="span_maqEscribir2 overflow-hidden rounded-md bottom-2  w-full z-90 absolute bg-gradient-to-r from-zinc-50 text-zinc-700 font-semibold  px-5 py-2 text-xs">Harry Potter App</figcaption>
         </figure>
     </div>
     <div class="grid gap-4">
         <figure class="max-w-full relative">
-            <iframe class="h-52 object-cover max-w-full rounded-lg" src="public/img/macbook-subtle-turn.mp4" allow="autoplay; fullscreen; picture-in-picture" alt=""/>
-            <figcaption class="bottom-5 lg:bottom-2 md:bottom-2 w-full z-90 absolute text-center  text-white font-semibold bg-black/50 px-5 py-2 text-xs">Claustrum Aqvae</figcaption>
+            <iframe class="h-52 object-cover max-w-full rounded-lg  opacity-50  hover:opacity-100 transition-opacity duration-300 " src="public/img/macbook-subtle-turn.mp4" allow="autoplay; fullscreen; picture-in-picture" alt=""/>
+            <figcaption class="span_maqEscribir2 overflow-hidden rounded-md top-1  w-full z-90 absolute bg-gradient-to-r from-zinc-50 text-zinc-700 font-semibold  px-5 py-2 text-xs">Claustrum Aquae</figcaption>
         </figure>
         <figure class="max-w-full relative">
-            <img class="h-auto max-w-full rounded-lg bg-zinc-200" src="public/img/Iphone 15 Screens Mockup.png" alt=""/>
-            <figcaption class="bottom-5  w-full z-90 absolute text-center  text-white font-semibold bg-black/50 px-5 py-2 text-xs">Claustrum Aqvae</figcaption>
+            <img class="h-auto max-w-full rounded-lg bg-zinc-200 opacity-50  hover:opacity-100 transition-opacity duration-300 " src="public/img/Iphone 15 Screens Mockup.png" alt=""/>
+            <figcaption class="span_maqEscribir2 overflow-hidden top-1 rounded-md w-full left-0 z-90 absolute bg-gradient-to-r from-zinc-50 text-zinc-700 font-semibold  px-5 py-2 text-xs">Claustrum Aqvae</figcaption>
         </figure>
         <figure class="max-w-full relative">
-            <img class="h-52 object-cover max-w-full rounded-lg" src="public/img/Mobile App Screen Mockup, Mosaic.png" alt=""/>
-            <figcaption class="bottom-5  w-full z-90 absolute text-center  text-white font-semibold bg-black/50 px-5 py-2 text-xs">The HolyWine</figcaption>
+            <img class="h-56 object-cover bg-zinc-200 opacity-40  hover:opacity-100 transition-opacity duration-300 max-w-full rounded-lg" src="public/img/Mobile App Screen Mockup, Mosaic.png" alt=""/>
+            <figcaption class="span_maqEscribir2 overflow-hidden rounded-md top-1  w-full left-0 z-90 absolute bg-gradient-to-r from-zinc-50  text-zinc-700 font-semibold  px-5 py-2 text-xs">The Holy Wine</figcaption>
         </figure>
     </div>
     <div class="grid gap-4">
