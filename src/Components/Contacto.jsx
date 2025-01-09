a830f1b2a9750c5170f601901902d74fd41287bb
81andie <133395577+81andie@users.noreply.github.com>
2025-01-09 15:55:54 +0100

    agregando un boton

--- src/Components/Contacto.jsx
+++ src/Components/Contacto.jsx
@@ -15,11 +15,11 @@ export const Contacto = () => {
             <img src="https://images.pexels.com/photos/4792728/pexels-photo-4792728.jpeg?auto=compress&cs=tinysrgb&w=600" class="h-96 w-full object-cover rounded-lg"></img>
           </div>
 
-          <div class="w-4/5 h-96">
+          <div class="w-4/5 h-96 ml-2">
           <h2 class="mb-4 text-2xl tracking-tight font-extrabold text-center text-zinc-700 font-mono">Contacto</h2>
           <p class="mb-8 lg:mb-10 font-light text-center text-gray-500 dark:text-gray-400 sm:text-base">Quieres accesoramiento, consejo o quieres transmitirme tu idea para tu web, ponte en contacto conmigo</p>
-          <div class="flex flex-row ">
-          <div class="flex flex-col w-full">
+          <div class="flex flex-row gap-2">
+          <div class="flex flex-col w-full ">
               <label for="email" class="block mb-1 text-xs font-medium text-gray-900 ">Your email</label>
               <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block  p-2.5 dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light " placeholder="name@flowbite.com" required/>
           </div>
@@ -30,7 +30,7 @@ export const Contacto = () => {
 
           </div>
           <textarea id="message" rows="4" class="mt-4 ml-7 block p-2 w-11/12 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500   dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
-          
+          <button type="submit" class="w-full bg-zinc-900 text-white p-1 rounded-sm mt-2">Enviar</button>
          
          
           </div>
