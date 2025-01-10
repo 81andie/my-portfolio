c5bc1358661b3f5e017947e57a9dc7e287fd4e3f
81andie <133395577+81andie@users.noreply.github.com>
2025-01-10 22:33:10 +0100

    guardando cambios en el darkMode

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,81 @@ import './index.css';
 import './Certificaciones.css';
 import './responsive.css';
 import './ErrorPage.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { RoutasPrincipales } from './routing/RoutasPrincipales';
 import { I18nextProvider } from 'react-i18next';
-import i18n from './Configuracion/i18n'; 
+import i18n from './Configuracion/i18n';
 import LanguageSelector from './Components/LanguageSelector';
 
+
+const Moon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-5 w-5"
+    viewBox="0 0 20 20"
+    fill="currentColor"
+  >
+    <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
+  </svg>
+);
+
+const Sun = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-5 w-5"
+    viewBox="0 0 20 20"
+    fill="currentColor"
+  >
+    <path
+      fillRule="evenodd"
+      d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
 
 
 
 function App() {
-  const [theme, setTheme] = useState('light');
 
-  const toggleTheme = () => {
-    setTheme(theme === 'light' ? 'dark' : 'light');
-  };
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    const theme = localStorage.getItem("theme");
+    if (theme === "dark") {
+      window.document.documentElement.classList.add("dark");
+      setIsDarkMode(true)
+    }
+
+  }, [])
+
+  const handleClick = () => {
+    if (isDarkMode) {
+      window.document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    } else {
+      localStorage.setItem("theme", "dark");
+      window.document.documentElement.classList.add("dark");
+    }
+    setIsDarkMode(!isDarkMode);
+
+  }
+
+
+
+
+
+
+
 
 
   return (
 
     <>
       <I18nextProvider i18n={i18n}>
-      <div className="App">
-      <div className= {theme === 'light' ? 'light-theme' : 'dark-theme' }>
-         
+        <div className=" dark:bg-slate-950 ">
+          <div>
+
             <div className="marquee-wrapper">
               <div className="marquee-content">
                 <p>_WEB <strong>DEVELOPER FRONTEND</strong>__<strong>H</strong>TML__ <strong>C</strong>SS__<strong>S</strong>ASS__<strong>J</strong>S__<strong>R</strong>EACT__<strong>A</strong>NGULAR___WEB <strong>DEVELOPER FRONTEND</strong>__<strong>H</strong>TML__ <strong>C</strong>SS__<strong>S</strong>ASS__<strong>J</strong>S__<strong>R</strong>EACT__<strong>A</strong>NGULAR__</p>
@@ -35,25 +86,30 @@ function App() {
             </div>
 
             <div className="flex justify-end">
-            <LanguageSelector />
-            <button className={theme} onClick={toggleTheme}>
-              <i className={theme === 'light' ? 'pi pi-sun' : 'pi pi-moon'}></i>
-              <br /> {theme === 'light' ? 'LIGHT THEME' : 'DARK THEME'}
-            </button>
-              
-            </div>
-           
-            
 
-            <RoutasPrincipales/>
-           
-           
+              <button className="w-10 bg-emerald-500"
+                onClick={handleClick}
+                aria-label={`Toggle ${isDarkMode ? "light" : "dark"} mode`}
+
+              >
+
+                {isDarkMode ? <Moon /> : <Sun />}
+              </button>
+              <LanguageSelector />
+
+            </div>
+
+
+
+            <RoutasPrincipales />
+
+
           </div>
         </div>
 
       </I18nextProvider>
-    
-   
+
+
 
     </>
   );
