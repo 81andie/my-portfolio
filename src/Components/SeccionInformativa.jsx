579214371ab8c526a4a55cabed3bee0196858638
81andie <133395577+81andie@users.noreply.github.com>
2025-01-13 17:55:28 +0100

    Mejorando la visibilidad de las fuentes en la seccionInformativa

--- src/Components/SeccionInformativa.jsx
+++ src/Components/SeccionInformativa.jsx
@@ -17,20 +17,20 @@ export const SeccionInformativa = () => {
                 <div className='icon_section text-center text-sm'>
 
                     <span><i className="pi pi-thumbs-up" style={{ color: '#e040a4' }}></i></span>
-                    <h1 className="titsec_Informacion text-xl">{t('sectionInformation.professionalism.title')}</h1>
+                    <h1 className="titsec_Informacion">{t('sectionInformation.professionalism.title')}</h1>
                     <p className="parrsec_Informacion">{t('sectionInformation.professionalism.description')}</p>
 
                 </div>
 
-                <div className='icon_section text-center'>
+                <div className='icon_section text-center text-sm'>
 
                     <span><i className="pi pi-question" style={{ color: '#e040a4' }}></i></span>
-                    <h1 className="titsec_Informacion">{t('sectionInformation.advice.title')}</h1>
+                    <h1 className="titsec_Informacion ">{t('sectionInformation.advice.title')}</h1>
                     <p className="parrsec_Informacion">{t('sectionInformation.advice.description')}</p>
 
                 </div>
 
-                <div className='icon_section text-center'>
+                <div className='icon_section text-center text-sm'>
                     <span><i className="pi pi-tablet" style={{ color: '#e040a4' }}></i></span>
                     <h1 className="titsec_Informacion">{t('sectionInformation.devices.title')}</h1>
                     <p className="parrsec_Informacion"> {t('sectionInformation.devices.description')}</p>
