142c8b1654684c46f27ca64394ba5b0ac497a684
81andie <133395577+81andie@users.noreply.github.com>
2025-01-13 17:33:31 +0100

    Creando un archivo css para la sección informativa,  y adaptando el archivo a tailwind, cambio tb a font-mono para seguir una misma linea de diseño

--- src/Components/SeccionInformativa.jsx
+++ src/Components/SeccionInformativa.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './SeccionInformativa.css';
 import { useTranslation } from 'react-i18next';
 
 
@@ -11,19 +12,17 @@ export const SeccionInformativa = () => {
 
         <>
 
-       
+            <div className="ml-2 mr-2 seccionInformacion w-full justify-center flex flex-col md:flex-row lg:flex-row h-auto mt-10 mb-20 gap-4 font-mono">
 
-            <div className="seccionInformacion">
-
-                <div className='icon_section'>
+                <div className='icon_section text-center text-sm'>
 
                     <span><i className="pi pi-thumbs-up" style={{ color: '#e040a4' }}></i></span>
-                    <h1 className="titsec_Informacion">{t('sectionInformation.professionalism.title')}</h1>
+                    <h1 className="titsec_Informacion text-xl">{t('sectionInformation.professionalism.title')}</h1>
                     <p className="parrsec_Informacion">{t('sectionInformation.professionalism.description')}</p>
 
                 </div>
 
-                <div className='icon_section'>
+                <div className='icon_section text-center'>
 
                     <span><i className="pi pi-question" style={{ color: '#e040a4' }}></i></span>
                     <h1 className="titsec_Informacion">{t('sectionInformation.advice.title')}</h1>
@@ -31,7 +30,7 @@ export const SeccionInformativa = () => {
 
                 </div>
 
-                <div className='icon_section'>
+                <div className='icon_section text-center'>
                     <span><i className="pi pi-tablet" style={{ color: '#e040a4' }}></i></span>
                     <h1 className="titsec_Informacion">{t('sectionInformation.devices.title')}</h1>
                     <p className="parrsec_Informacion"> {t('sectionInformation.devices.description')}</p>
