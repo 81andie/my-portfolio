ec106b34c60ae03878a25139e385edfc553b04c2
81andie <133395577+81andie@users.noreply.github.com>
2025-01-13 12:32:52 +0100

    puliendo los estilos del footer(adequando el flex en los divs), eliminando tb los iconos de prueba,

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,20 +6,18 @@ export const Footer = () => {
   let img = "img/logo2.png";
   return (
     <>
-      <div className="flex justify-center md:flex-row ">
-      <img src={img} className='w-20 h-20' alt="logo-imagen" />
+      <div className="flex flex-col md:flex-row  lg:flex-row justify-center items-center gap-2">
+      <img src={img} className='w-28 h-28 ' alt="logo-imagen" />
 
-        <div className="icons_social">
-        
-          <i className="pi pi-instagram" style={{ color: '#e040a4' }}></i>
-          <i className="pi pi-facebook" style={{ color: '#e040a4' }}></i>
-          <i className="pi pi-github" style={{ color: '#e040a4' }}></i>
-          <i className="pi pi-telegram" style={{ color: '#e040a4' }}></i>
+        <div className="icons_social flex flex-row text-3xl justify-center">
+    
+          <i className="pi pi-github mr-1" style={{ color: '#e040a4' }}></i>
+          <i className="pi pi-linkedin" style={{ color: '#e040a4' }}></i>
           </div>
 
-        <div className="contacto_footer">
+        <div className="text-center mb-2">
        
-        <h3 className="dev_nombre"> Diseñadora página web y desarrollor Web:<br/> Andrea Hernández <br/>©2024 Andrea Hernández | All Rights Reserved </h3>
+        <h3 className="text-slate-500"> Diseñadora página web y desarrollor Web:<br/> Andrea Hernández <br/>©2024 Andrea Hernández | All Rights Reserved </h3>
         
         
        
