4b933fe532a926a48d62b112d3bdab6e63b4b06e
81andie <133395577+81andie@users.noreply.github.com>
2025-01-16 17:53:38 +0100

    creando un array para los proyectos

--- src/Components/Proyectos.jsx
+++ src/Components/Proyectos.jsx
@@ -1,55 +1,77 @@
 import React from 'react'
 
 export const Proyectos = () => {
+
+  const projects = [
+    {
+      title: "Harry Potter App",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      images: [
+        "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg",
+        "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg",
+        "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg",
+        "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg",
+      ],
+      technologies: [
+        {
+          name: "JavaScript",
+          icon: "https://miro.medium.com/v2/resize:fit:1200/1*lhfGTouqSQ-fx7PRXaFI-Q.png",
+        },
+        {
+          name: "Tailwind CSS",
+          icon: "https://picperf.io/https://laravelnews.s3.amazonaws.com/images/tailwindcss-1633184775.jpg",
+        },
+        {
+          name: "PrimeNG",
+          icon: "https://www.primetek.com.tr/img/primeng.jpg",
+        },
+      ],
+    }
+   
+  ];
   return (
-    
-<>
-<div class="flex flex-col md:flex-row lg:flex-row gap-4 p-3">
-    <div class="grid gap-4">
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg" alt=""/>
+
+    <>
+
+<div className="flex flex-col justify-center ">
+      {projects.map((project, index) => (
+        <div
+          key={index}
+          className="flex flex-col md:flex-row lg:flex-row justify-center rounded-lg "
+        >
+          <div className="grid md:grid-cols-2 lg:grid-cols-2 grid-cols-1 gap-2 p-3">
+            {project.images.map((image, i) => (
+              <img
+                key={i}
+                className="h-auto w-full md:w-52 lg:w-52 rounded-lg "
+                src={image}
+                alt={`Project ${project.title} - Image ${i + 1}`}
+              />
+            ))}
+          </div>
+          <div className="flex flex-col p-1 ">
+            <p className="text-xl font-mono">Titulo:</p>
+            <p className="text-3xl p-1 mt-2 mb-2">{project.title}</p>
+            <p className="text-xl font-mono">Lenguajes y frameworks:</p>
+            <div className="flex flex-row ">
+              {project.technologies.map((tech, i) => (
+                <img
+                  key={i}
+                  className="h-10 w-auto"
+                  src={tech.icon}
+                  alt={tech.name}
+                  title={tech.name}
+                />
+              ))}
+            </div>
+            <p className="text-md">{project.description}</p>
+          </div>
         </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg" alt=""/>
-        </div>
-       
-       
+      ))}
     </div>
-    <div class="grid gap-4">
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt=""/>
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg" alt=""/>
-        </div>
-       
-    </div>
 
-    <div>
-      <p class="text-xl font-mono">Titulo:</p>
-      <p class="text-3xl p-2 mt-2 mb-2">Harry Potter App</p>
-
-      <div className="flex flex-col">
-      <p class="text-xl font-mono">Lenguages y frameworks:</p>
-      <div class="flex flex-row">
-       <img src="https://miro.medium.com/v2/resize:fit:1200/1*lhfGTouqSQ-fx7PRXaFI-Q.png" className="h-10 w-auto"></img>
-       <img src="https://picperf.io/https://laravelnews.s3.amazonaws.com/images/tailwindcss-1633184775.jpg" className="h-10 w-auto"></img>
-       <img src="https://www.primetek.com.tr/img/primeng.jpg" className="h-10 w-auto"></img>
-      </div>
-
-      <div class="">
-        <p class="w-96">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-      </div>
      
-     
-    </div>
-  
-     
-    </div>
-
-   
-   
-</div>
-</>
+    </>
   )
 }
