cf858646fe0c518dba31067cdde2cf22d7acd3d9
81andie <133395577+81andie@users.noreply.github.com>
2025-01-16 20:30:07 +0100

    Probando como se ven los proyectos a medida que los voy añadiendo en el archivo js

--- src/Components/Proyectos.jsx
+++ src/Components/Proyectos.jsx
@@ -1,77 +1,51 @@
 import React from 'react'
+import { projects } from './ProyectosData';
 
 export const Proyectos = () => {
 
-  const projects = [
-    {
-      title: "Harry Potter App",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      images: [
-        "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg",
-        "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg",
-        "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg",
-        "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg",
-      ],
-      technologies: [
-        {
-          name: "JavaScript",
-          icon: "https://miro.medium.com/v2/resize:fit:1200/1*lhfGTouqSQ-fx7PRXaFI-Q.png",
-        },
-        {
-          name: "Tailwind CSS",
-          icon: "https://picperf.io/https://laravelnews.s3.amazonaws.com/images/tailwindcss-1633184775.jpg",
-        },
-        {
-          name: "PrimeNG",
-          icon: "https://www.primetek.com.tr/img/primeng.jpg",
-        },
-      ],
-    }
-   
-  ];
+
   return (
 
     <>
 
-<div className="flex flex-col justify-center ">
-      {projects.map((project, index) => (
-        <div
-          key={index}
-          className="flex flex-col md:flex-row lg:flex-row justify-center rounded-lg "
-        >
-          <div className="grid md:grid-cols-2 lg:grid-cols-2 grid-cols-1 gap-2 p-3">
-            {project.images.map((image, i) => (
-              <img
-                key={i}
-                className="h-auto w-full md:w-52 lg:w-52 rounded-lg "
-                src={image}
-                alt={`Project ${project.title} - Image ${i + 1}`}
-              />
-            ))}
-          </div>
-          <div className="flex flex-col p-1 ">
-            <p className="text-xl font-mono">Titulo:</p>
-            <p className="text-3xl p-1 mt-2 mb-2">{project.title}</p>
-            <p className="text-xl font-mono">Lenguajes y frameworks:</p>
-            <div className="flex flex-row ">
-              {project.technologies.map((tech, i) => (
+      <div className="flex flex-col justify-center ">
+        {projects.map((project, index) => (
+          <div
+            key={index}
+            className="flex flex-col md:flex-row lg:flex-row justify-center rounded-lg "
+          >
+            <div className="grid md:grid-cols-2 lg:grid-cols-2 grid-cols-1 gap-2 p-3">
+              {project.images.map((image, i) => (
                 <img
                   key={i}
-                  className="h-10 w-auto"
-                  src={tech.icon}
-                  alt={tech.name}
-                  title={tech.name}
+                  className="h-auto object-cover max-w-full md:w-52 lg:w-52 rounded-lg "
+                  src={image}
+                  alt={`Project ${project.title} - Image ${i + 1}`}
                 />
               ))}
             </div>
-            <p className="text-md">{project.description}</p>
+            <div className="flex flex-col p-1 ">
+              <p className="text-xl font-mono">Titulo:</p>
+              <p className="text-3xl p-1 mt-2 mb-2">{project.title}</p>
+              <p className="text-xl font-mono">Lenguajes y frameworks:</p>
+              <div className="flex flex-row ">
+                {project.technologies.map((tech, i) => (
+                  <img
+                    key={i}
+                    className="h-10 w-auto"
+                    src={tech.icon}
+                    alt={tech.name}
+                    title={tech.name}
+                  />
+                ))}
+              </div>
+              <p className="text-md">{project.description}</p>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
 
-     
+
     </>
   )
 }
---
+++ src/Components/proyectosData.js
@@ -0,0 +1,60 @@
+export const projects = [
+    {
+      title: "Harry Potter App",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      images: [
+        "./imgProjects/hp1.jpg",
+        "./imgProjects/hp3.jpg",
+        "./imgProjects/hp5.jpg",
+        "./imgProjects/hp2.jpg",
+        
+
+      ],
+      technologies: [
+        {
+          name: "JavaScript",
+          icon: "https://miro.medium.com/v2/resize:fit:1200/1*lhfGTouqSQ-fx7PRXaFI-Q.png",
+        },
+        {
+          name: "Tailwind CSS",
+          icon: "https://picperf.io/https://laravelnews.s3.amazonaws.com/images/tailwindcss-1633184775.jpg",
+        },
+        {
+          name: "PrimeNG",
+          icon: "https://www.primetek.com.tr/img/primeng.jpg",
+        },
+      ],
+    },
+
+    {
+        title: "México Guide",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        images: [
+          "./imgProjects/mex1.jpg",
+          "./imgProjects/mex2.jpg",
+          "./imgProjects/mex3.jpg",
+          "./imgProjects/mex5.jpg",
+          
+  
+        ],
+        technologies: [
+          {
+            name: "JavaScript",
+            icon: "https://th.bing.com/th/id/R.63cf9c6f8153aef1e884b7ae2815e06d?rik=CfiMB1gBwpe79Q&pid=ImgRaw&r=0",
+          },
+          {
+            name: "Tailwind CSS",
+            icon: "https://picperf.io/https://laravelnews.s3.amazonaws.com/images/tailwindcss-1633184775.jpg",
+          },
+
+          {
+            name: "Google APIs",
+            icon: "https://logodix.com/logo/874775.jpg",
+          },
+         
+        ],
+      }
+
+  ];
