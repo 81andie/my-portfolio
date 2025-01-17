cbe21241169656edf13e79f84d357a1e3286b58a
81andie <133395577+81andie@users.noreply.github.com>
2025-01-17 21:05:51 +0100

    Añadiendo nuevos proyectos a la sección

--- src/Components/proyectosData.js
+++ src/Components/proyectosData.js
@@ -1,5 +1,57 @@
 export const projects = [
   {
+    title: "Traffic Valéncia",
+    description:
+      "Traffic Valencia se ha desarrollado con JS, geocaliza todas las cámaras de tráfico de la área metropolitana de Valéncia, pudiendo ver a tiempo real lo que graban las cámaras.",
+    
+    description1:"Los datos para la geolocalización de las cámaras, los aporta el portal de datos abiertos del Ayuntamiento de Valéncia. Contiene también una pequeña libreria 'leaflet-control-geocoder', para poder geolocalizar algún destino en concreto",
+      
+    
+    
+      images: [
+        "./imgProjects/traffic.jpg",
+        "./imgProjects/traffic6.jpg",
+      "./imgProjects/traffic1.jpg",
+      "./imgProjects/traffic5.jpg",
+     
+      "./imgProjects/traffic3.jpg",
+      "./imgProjects/traffic4.jpg",
+     
+      
+      
+
+    ],
+    technologies: [
+      {
+        name: "JavaScript",
+        icon: "https://th.bing.com/th/id/R.63cf9c6f8153aef1e884b7ae2815e06d?rik=CfiMB1gBwpe79Q&pid=ImgRaw&r=0",
+      },
+      {
+        name: "Tailwind CSS",
+        icon: "https://picperf.io/https://laravelnews.s3.amazonaws.com/images/tailwindcss-1633184775.jpg",
+      },
+
+      {
+        name: "Leaflet",
+        icon: "https://www.marefa.org/w/images/thumb/1/13/Leaflet_logo.svg/800px-Leaflet_logo.svg.png?20231123133844",
+      },
+     
+    ],
+
+    url:[
+      "https://github.com/81andie/TraficoValencia"
+    ],
+
+    url1:[
+      "https://81andie.github.io/TraficoValencia/Index.html"
+    ]
+
+  },
+  
+  
+  
+  
+  {
     title: "México Guide",
     description:
       "México Guide, es una guia interactiva con la geolocalización aportados por el Instituto de Geografía de la Unam(México), aprovechando la plataforma IDEA lleva la renderización por capas, de diferentes metadatos.",
@@ -24,6 +76,11 @@ export const projects = [
       {
         name: "Tailwind CSS",
         icon: "https://picperf.io/https://laravelnews.s3.amazonaws.com/images/tailwindcss-1633184775.jpg",
+      },
+
+      {
+        name: "MapLibre",
+        icon: "https://jquery-plugins.net/image/plugin/maplibre-gl-open-source-mapping-libraries-for-web-and-mobile-applications.png",
       },
 
       {
