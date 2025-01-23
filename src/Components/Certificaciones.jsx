4884eb15c93e702d7490963e2d321ada1f08db9e
81andie <133395577+81andie@users.noreply.github.com>
2025-01-23 20:48:09 +0100

    Ajustando la sección certificaciones

--- src/Components/Certificaciones.jsx
+++ src/Components/Certificaciones.jsx
@@ -1,73 +1,65 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next';
+import { certifications } from './certifications';
+
 
 
 export const Certificaciones = () => {
+
+
+
+
   const { t } = useTranslation();
+
+
 
   return (
     <>
-  
+
       <h1 className="certificaciones_title">{t('header.certifications')}</h1>
       <div className="container_certificaciones">
 
-        <div className="card_google">
-          <h2>{t('certificationsGoogle.june')}</h2>
-          <h3>{t('certificationsGoogle.course')} <br /> {t('certificationsGoogle.coursetitle')}</h3>
-          <img src="https://www.emprendedorinteligente.com/wp-content/uploads/2019/11/header-activate-logo-lg-1024x286.png" className="logo_google" />
-          <details>
-            <summary>{t('certificationsGoogle.summary')}</summary>
-            <li>{t('certificationsGoogle.topics.history')}</li>
-            <li>{t('certificationsGoogle.topics.functioning')}</li>
-            <li>{t('certificationsGoogle.topics.writing')}</li>
-            <li>{t('certificationsGoogle.topics.publication')}</li>
-            <li>{t('certificationsGoogle.topics.page')}</li>
-          </details>
+        {certifications.map((cer, index) => (
+          <div className="card_google" key={index}
+            style={{
+              backgroundImage: `url(${cer.backgroundImage})`,
 
-        </div>
+            }}
 
-        <div className="card_google1">
-          <h2>{t('certificationsGoogle1.july')}</h2>
-          <h3>{t('certificationsGoogle1.course')} <br /> {t('certificationsGoogle1.coursetitle')}</h3>
-          <img src="https://www.emprendedorinteligente.com/wp-content/uploads/2019/11/header-activate-logo-lg-1024x286.png" className="logo_google" />
-          <details>
-            <summary>{t('certificationsGoogle.summary')}</summary>
-            <li>{t('certificationsGoogle1.topics.presentationDefinition')}</li>
-            <li>{t('certificationsGoogle1.topics.elementsPresentationDefinition')}</li>
-            <li>{t('certificationsGoogle1.topics.webSuccessAchievement')}</li>
-            <li>{t('certificationsGoogle1.topics.webLayout')}</li>
-          </details>
+          >
 
-        </div>
+            <div
+              style={{
+                content: '""',
+                opacity: 0.6,
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: '100%',
+                background: cer.gradient,
+                pointerEvents: 'none'
+              }}
+            ></div>
 
-        <div className="card_google3">
-          <h2>{t('certificationsUdemy.august')}</h2>
-          <h3>{t('certificationsUdemy.course')}</h3>
-          <img src="https://logosmarcas.net/wp-content/uploads/2021/11/Udemy-Logo-650x366.png" className="logo_google" />
-          <details>
-            <summary>{t('certificationsGoogle.summary')}</summary>
-            <li>{t('certificationsUdemy.topics.htmlFundamentals')}</li>
-            <li>{t('certificationsUdemy.topics.cssFundamentals')}</li>
-            <li>{t('certificationsUdemy.topics.layouts')}</li>
-            <li>{t('certificationsUdemy.topics.components')}</li>
-            <li>{t('certificationsUdemy.topics.projects')}</li>
-          </details>
-        </div>
+            <h2>{cer.course}</h2>
+            <img
+              src={cer.image}
 
-        <div className="card_google2">
-          <h2>{t('certificationsFreeCodeCamp.certification_info.title')}</h2>
-          <h3>{t('certificationsFreeCodeCamp.certification_info.subtitle')}</h3>
-          <img src="https://www.arnoldgelacio.com/images/skills/logo-freecodecamp.png" className="logo_google" />
-          <details>
-            <summary>{t('certificationsGoogle.summary')} 300h</summary>
-            <li>{t('certificationsFreeCodeCamp.certification_info.summary.details.0')}</li>
-            <li>{t('certificationsFreeCodeCamp.certification_info.summary.details.1')}</li>
-            <li>{t('certificationsFreeCodeCamp.certification_info.summary.details.2')}</li>
-            <li>{t('certificationsFreeCodeCamp.certification_info.summary.details.3')}</li>
-          </details>
+              className="logo_google"
+              alt="Certificación"
+            />
+            <details>
+              <summary>{cer.summary}</summary>
+              {cer.topics && cer.topics.map((topic, i) => (
+                <li key={i}>{topic}</li>
+              ))
 
-        </div>
+              }
 
+            </details>
+          </div>
+        ))}
 
       </div>
     </>
---
+++ src/Components/certifications.js
@@ -0,0 +1,77 @@
+export const certifications = [
+    {
+      "id": 1,
+      "month": "June 2023",
+      "course": "Curs d'introducció al desenvolupament web: HTML i CSS(I)",
+      "institution": "Google Activate",
+      "image": "https://www.emprendedorinteligente.com/wp-content/uploads/2019/11/header-activate-logo-lg-1024x286.png",
+      "backgroundImage": "https://img.freepik.com/vector-gratis/estilo-fondo-monocromatico-blanco_23-2148995968.jpg?t=st=1712941753~exp=1712945353~hmac=df34bde36b9ebc7f85b636baeef4df2de28282f61fb1d61457828075cf0c7a4d&w=900",
+      "summary": "Resum del curs",
+      "gradient": "linear-gradient(to bottom, rgba(167, 184, 227, 0.269), rgba(16, 90, 239, 0.358))",
+      "topics": [
+        "Història de la Web",
+        "Com funciona la Web",
+        "Com s'escriu per la Web",
+        "Publicació d'un lloc web",
+        "Creació d'una pàgina web"
+      ]
+    },
+    {
+      "id": 2,
+      "month": "Juliol 2023",
+      "course": "Curs d'introducció al desenvolupament web HTML and CSS(II)",
+      "institution": "Google Activate",
+      "image": "https://example.com/js-logo.png",
+      "backgroundImage": "https://img.freepik.com/vector-gratis/fondo-monocromatico-blanco-estilo-papel_23-2148996213.jpg?t=st=1712941497~exp=1712945097~hmac=d92a0f0d76ed68e8d2da654fe6bc67fb7cddb5dad5a8d8c2a73d012060773d8c&w=900",
+      "summary": "Resum del curs",
+      "topics": [
+        "Com es defineix la presentació d'una pàgina web",
+        "Com es defineix la presentació dels elements d'una pàgina web",
+        "Com aconseguir que una pàgina web tingui èxit",
+        "Com maquetar una pàgina web",
+      
+      ]
+    },
+
+    {
+        "id": 3,
+        "month": "Setembre 2023",
+        "course": "Disseny responsiu Web",
+        "institution": "FreeCodeCamp",
+        "image": "https://th.bing.com/th/id/OIP.DjzcfrlwLev0VKX5KpppDgHaHa?rs=1&pid=ImgDetMain",
+        "backgroundImage": "https://img.freepik.com/vector-gratis/fondo-monocromatico-blanco-estilo-papel_23-2148996213.jpg?t=st=1712941497~exp=1712945097~hmac=d92a0f0d76ed68e8d2da654fe6bc67fb7cddb5dad5a8d8c2a73d012060773d8c&w=900",
+        "summary": "Resum del curs",
+        "topics": [
+          "HTML",
+          "CSS",
+          "FlexBox",
+          "Diseny Responsiu",
+        
+        ]
+      },
+
+      {
+        "id": 4,
+        "month": "Setembre 2024",
+        "course": "Algoritmes i estructura de dades de JavaScript",
+        "institution": "FreeCodeCamp",
+        "image": "https://th.bing.com/th/id/OIP.DjzcfrlwLev0VKX5KpppDgHaHa?rs=1&pid=ImgDetMain",
+        "backgroundImage": "https://img.freepik.com/vector-gratis/fondo-monocromatico-blanco-estilo-papel_23-2149016321.jpg?t=st=1737651811~exp=1737655411~hmac=75a996331f98a6455eeea8adf1f3b00d919689d2277a12479ea4656c7dbd5a95&w=826",
+        "summary": "Resum del curs",
+        "topics": [
+          "JavaScript Básic",
+          "ES6",
+          "Expresions Regulars",
+          "Depuració",
+          "Estructures de dades básiques",
+          "Programació d'algoritmes básics",
+          "Programació orientada a objectes",
+          "Programació funcional",
+          "Programació d'algoritmes intermedis",
+          "Algoritmes de JavaScript y proyectes"
+        
+    
+        ]
+      }
+  ]
+  
