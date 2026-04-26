78639e7c84a7888609b45412be4e2021f29c4494
81andie <133395577+81andie@users.noreply.github.com>
2026-04-26 14:15:18 +0200

    Quitando el formulario de la sección de contactos y añadiendo el email

--- src/Components/Contacto.jsx
+++ src/Components/Contacto.jsx
@@ -6,9 +6,7 @@ export const Contacto = () => {
 
   const { t } = useTranslation();
 
-
   return (
-
     <>
       <form className="mb-20 mr-2 ml-2 mt-32">
         <div className="flex flex-col  lg:flex-row md:flex-row">
@@ -17,38 +15,12 @@ export const Contacto = () => {
           </div>
 
           <div className="md:w-4/5 lg:w-4/5 h-96 md:ml-4 lg:ml-8 lg:mr-8">
-          <h2 className="mb-4 text-2xl tracking-tight font-extrabold text-center text-zinc-700 font-mono">{t('Contact.contact')}</h2>
-          <p className="mb-8 lg:mb-10 font-light text-center text-gray-500 dark:text-gray-400 sm:text-base">{t('Contact.you wants')}</p>
-          <div className="flex flex-row gap-2">
-          <div className="flex flex-col w-full ">
-              <label htmlFor="email" className="block mb-1 text-xs font-medium text-gray-900 ">{t('Contact.email')}</label>
-              <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block  p-2.5 dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light " placeholder="example@gmail.com" required/>
+            <h2 className="mb-4 text-2xl tracking-tight font-extrabold text-center text-zinc-900 font-mono">{t('Contact.contact')}</h2>
+            <p className="mb-8 lg:mb-10 font-light text-center text-gray-800 dark:text-gray-100 sm:text-base">{t('Contact.you wants')}</p>
+            <p className="mb-8 lg:mb-10 text-2xl font-bold  text-center text-gray-800 dark:text-gray-400">pianistgirl81@gmail.com</p>
           </div>
-          <div className="flex flex-col w-full">
-          <label htmlFor="name" className="block mb-1 text-xs font-medium text-gray-900 ">{t('Contact.name')}</label>
-          <input type="text" id="text" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block  p-2.5 dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light " placeholder={t('Contact.NameEmail')} required/>
-          </div>
-
-          </div>
-          <div className="w-full ">
-          <textarea id="message" rows="4" className="mt-4  block p-2 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500   dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={t('Contact.you want')}></textarea>
-          <button type="submit" className=" bg-zinc-900 text-white p-1 rounded-sm mt-2 w-28 h-10">{t('Contact.Submit')}</button>
-          </div>
-         
-         
-         
-          </div>
-
         </div>
-
       </form>
-
-
-
-
-
-
-
     </>
   )
 }
